8cfc0bc2ce03913fa336a7f25fdfc262593fc765
Suji Hwang <soojiya2003@gmail.com>
2025-04-03 15:08:11 +0900

    memo 1000자 제한

---
+++ js/memo.js
@@ -0,0 +1,16 @@
+window.document.addEventListener("DOMContentLoaded", () => {
+  const memoTitle = document.querySelector(".memo-title");
+  const memoField = document.querySelector("#memo-text-box");
+
+  memoTitle.addEventListener("click", () => {
+    memoField.focus();
+  });
+
+  let count = 0;
+  memoField.addEventListener("keydown", (e) => {
+    count++;
+    if (count > 1000) {
+      alert("memo에는 1,000자 이상 입력할 수 없습니다. ");
+    }
+  });
+});
