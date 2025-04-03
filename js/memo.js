15dabeefe225057e5bc93e36766cb5176e356125
Suji Hwang <soojiya2003@gmail.com>
2025-04-03 16:31:10 +0900

    메모 포커스만 남김

--- js/memo.js
+++ js/memo.js
@@ -6,12 +6,12 @@ window.document.addEventListener("DOMContentLoaded", () => {
     memoField.focus();
   });
 
-  let count = 0;
-  memoField.addEventListener("keydown", (e) => {
-    count++;
-    if (count > 1000) {
-      alert("memo에는 1,000자 이상 입력할 수 없습니다. ");
-    }
-    //저장
-  });
+  //   let count = 0;
+  //   memoField.addEventListener("keydown", (e) => {
+  //     count++;
+  //     if (count > 1000) {
+  //       alert("memo에는 1,000자 이상 입력할 수 없습니다. ");
+  //     }
+  //     //저장
+  //   });
 });
