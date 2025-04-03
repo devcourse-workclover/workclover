07956ef44ecfd9ea8b52421b87048f911a5f7311
Suji Hwang <soojiya2003@gmail.com>
2025-04-03 16:23:16 +0900

    메모 저장 기능 전

--- js/memo.js
+++ js/memo.js
@@ -12,11 +12,6 @@ window.document.addEventListener("DOMContentLoaded", () => {
     if (count > 1000) {
       alert("memo에는 1,000자 이상 입력할 수 없습니다. ");
     }
+    //저장
   });
-
-  //memo 새로 고침 유지 - enter 나 blur 됐을 때 local 저장(?)
-  const saveMemoText = function () {};
-
-  memoField.addEventListener("blur", () => {});
-  memoField.addEventListener("enter", () => {});
 });
