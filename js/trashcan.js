3374481be38aaef274a31bb3e379ee2b4cc2967f
Suji Hwang <soojiya2003@gmail.com>
2025-04-04 03:14:24 +0900

    feat: 리스트 구현, 삭제

--- js/trashcan.js
+++ js/trashcan.js
@@ -1,6 +1,10 @@
+import { getTrashDocuments } from "../apis/getTrashDocuments.js";
+import { completeDeleteDocuments } from "../apis/completeDeleteDocuments.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   const trashcanButton = document.querySelector(".menubar-trashbin");
   const trashTitle = document.querySelector(".menubar-trashbin span");
+  const trashList = document.querySelector(".trash-list");
 
   trashcanButton.addEventListener("mouseenter", (e) => {
     trashTitle.style.borderBottom = "solid";
@@ -9,21 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
     trashTitle.style.borderBottom = "none";
   });
 
-  //리스트에 삭제된 리스트 get
-  // trashcanButton.addEventListener("click", () => {
-  //   const trashList = document.querySelector(".trashList");
-  //   if ((trashList.style.display = "none")) {
-  //     trashList.style.display = "block";
-  //   } else if ((trashList.style.display = "block")) {
-  //     trashList.style.display = "none";
-  //   }
-  // });
-  const dialog = document.querySelector("dialog");
+  async function viewTrashList() {
+    trashList.innerHTML = "";
+    const trashData = await getTrashDocuments();
+    trashData.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerText = item.title;
+      const restore = document.createElement("button");
+      const completeDelete = document.createElement("button");
+      restore.innerText = "복원";
+      completeDelete.innerText = "삭제";
+      const div = document.createElement("div");
+      div.append(restore, completeDelete);
+      li.append(div);
+      trashList.appendChild(li);
+
+      completeDelete.addEventListener("click", async () => {
+        await completeDeleteDocuments(item.id);
+        viewTrashList();
+      });
+    });
+    console.log(trashData);
+  }
+
+  const dialog = document.querySelector(".trash-modal");
   trashcanButton.addEventListener("click", () => {
     dialog.showModal();
-    // GET 받아오기
+    viewTrashList();
   });
-  dialog.addEventListener("click", () => {
-    dialog.close();
+  dialog.addEventListener("click", (e) => {
+    if (e.taraget === dialog) dialog.close();
   });
+
+  // 복원
+
+  // 완전삭제
 });
