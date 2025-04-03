fb78fdc52ab4d4af2bacce631f5924cdc78416c5
run3go <qkrwjdtn09@gmail.com>
2025-04-03 15:26:54 +0900

    feat: 문서 추가 기능 구현

---
+++ apis/addDocument.js
@@ -0,0 +1,20 @@
+const BASE_URL = "https://kdt-api.fe.dev-cos.com";
+const USERNAME = "workclover";
+
+export async function addDocument(parent = null) {
+  const response = await fetch(`${BASE_URL}/documents`, {
+    method: "POST",
+    headers: {
+      "x-username": USERNAME,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "새 페이지",
+      parent,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error("Network response is not ok!");
+  }
+  return await response.json();
+}
