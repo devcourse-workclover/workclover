dd7096c65aae6cf72994d9fd99cfd09f45b6c73f
Jungyu Park <parkjungyu09@gmail.com>
2025-04-04 11:41:13 +0900

    feat: 메모 내용 localstorage에 저장 및 호출 기능 추가

--- js/memo.js
+++ js/memo.js
@@ -1,17 +1,18 @@
 window.document.addEventListener("DOMContentLoaded", () => {
   const memoTitle = document.querySelector(".memo-title");
-  const memoField = document.querySelector("#memo-text-box");
+  const memoField = document.getElementById("memo-text-box");
+
+  const savedMemo = localStorage.getItem("memoText");
+  if (savedMemo) {
+    memoField.value = savedMemo;
+  }
 
   memoTitle.addEventListener("click", () => {
     memoField.focus();
   });
 
-  //   let count = 0;
-  //   memoField.addEventListener("keydown", (e) => {
-  //     count++;
-  //     if (count > 1000) {
-  //       alert("memo에는 1,000자 이상 입력할 수 없습니다. ");
-  //     }
-  //     //저장
-  //   });
+  memoField.addEventListener("input", (event) => {
+    const text = event.target.value;
+    localStorage.setItem("memoText", text);
+  });
 });
